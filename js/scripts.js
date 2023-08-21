7fb18520f7976f198faa3591c2f97f14b15dc2ca
manuggetts <manuggets@hotmail.com>
2023-08-21 17:34:28 -0300

    começo do js

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button");
+
+class Calculator {
+    constructor(previousOperationText, currentOperationText) {
+        this.previousOperationText = previousOperationText;
+        this.currentOperationText = currentOperationText;
+        this.currentOperation = "";
+    }
+    // adicionar digito na tela da calculadora
+    addDigit(digit) {
+
+        this.currentOperation = digit;
+        this.updateScreen();
+    }
+
+    // mudar o valor da tela da calculadora
+    updateScreen() {
+        this.currentOperationText.innerText += this.currentOperation;
+    }
+}
+
+const calc = new Calculator(previousOperationText, currentOperationText);
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        const value = e.target.innerText;
+
+        if (+value >= 0 || value === ".") {
+            calc.addDigit(value);
+        } else {
+            console.log("Op: " + value);
+        }
+    });
+});
